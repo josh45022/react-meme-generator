84a1bdbfb15cb40c305e3dc840bb3a50c4ecb8ce
Joshua Benbaba <joshuabenbaba@Marys-MacBook-Pro-3.local>
2020-12-16 13:40:23 -0700

    sean is helping me

---
+++ react-meme-generator/src/Meme.js
@@ -0,0 +1,12 @@
+import React from "react"
+function Meme(props) {
+    returns(
+        <div>
+            <div></div>
+            <img src={}/>
+            <div></div>
+            <button>Delete</button>
+        </div>
+    )
+}
+export default Meme
--- react-meme-generator/src/Preview.js
+++ react-meme-generator/src/Preview.js
@@ -1,3 +1,14 @@
 import React from "react"
 
-export default
+function Preview(props) {
+    return(
+        <div>
+            <div></div>
+            <img src={}/>
+            <div></div>
+            <button>Add To List</button>
+        </div>
+    )
+}
+
+export default Preview
